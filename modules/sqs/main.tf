4d5f5257350b9ba34c42fdde57c2d2ddb6f784af
Vanderson Nunes <vann.nunes@gmail.com>
2024-01-23 20:39:52 -0300

    Infra serviço pagamentos

--- modules/sqs/main.tf
+++ modules/sqs/main.tf
@@ -1,5 +1,6 @@
 locals {
   producao_queue_name = "fila-producao"
+  pagamentos_queue_name = "pagamentos"
 }
 
 
@@ -31,5 +32,34 @@ resource "aws_sqs_queue_redrive_allow_policy" "producao-queue-redrive-policy" {
     redrivePermission = "byQueue",
     sourceQueueArns   = [aws_sqs_queue.producao-dlq-queue.arn]
   })
-  
+}
+
+# ---- Pagamentos Queue ----
+
+# Create Pagamentos SQS dlq Queue
+resource "aws_sqs_queue" "pagamentos-dlq-queue" {
+  name = "${local.pagamentos_queue_name}-dlq"
+  tags = {
+    Name = "${local.pagamentos_queue_name}-dlq"
+    application = var.pagamentos_application_tag_name
+  }
+}
+
+# Create Pagamentos SQS Queue
+resource "aws_sqs_queue" "pagamentos-queue" {
+  name = local.pagamentos_queue_name
+  tags = {
+    Name = local.pagamentos_queue_name
+    application = var.pagamentos_application_tag_name
+  }
+}
+
+# Create Redrive Pagamentos Queue policy
+resource "aws_sqs_queue_redrive_allow_policy" "pagamentos-queue-redrive-policy" {
+  queue_url = aws_sqs_queue.pagamentos-queue.id
+
+  redrive_allow_policy = jsonencode({
+    redrivePermission = "byQueue",
+    sourceQueueArns   = [aws_sqs_queue.pagamentos-dlq-queue.arn]
+  })
 }
